c451bf95a2e34898312b4b1a7b57f5ed5cf653df
hvl052k2 <hvl052k2@gmail.com>
2023-02-01 10:24:42 +0700

    loc data cho trang Recent

---
+++ .history/src/screens/Recent_20230201101251.js
@@ -0,0 +1,48 @@
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+
+import SearchBar from "../components/SearchBar";
+import SongItem from "../components/SongItem";
+import { songData } from "../../data/songData";
+
+const Recent = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar></StatusBar>
+      {/* thanh tìm kiếm */}
+      <SearchBar />
+
+      {/* Title */}
+      <View
+        style={{
+          width: "100%",
+          height: 60,
+          backgroundColor: "#ccc",
+          justifyContent: "center",
+          paddingHorizontal: 20,
+        }}
+      >
+        <Text style={{ fontSize: 30, fontWeight: "500" }}>Bài hát gần đây</Text>
+      </View>
+
+      {/* Danh sách bài hát */}
+      <FlatList
+        style={{ flex: 1 }}
+        data={songData}
+        renderItem={({ item }) => <SongItem info={item} />}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Recent;
+
+const styles = StyleSheet.create({});
