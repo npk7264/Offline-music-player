15ccc5f52c2422ad3a8725b18f30529f31268c59
hvl052k2 <hvl052k2@gmail.com>
2023-02-02 14:49:59 +0700

    luu playlist vao Async Storage

--- src/screens/Playlist.js
+++ src/screens/Playlist.js
@@ -14,9 +14,40 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import SearchBar from "../components/SearchBar";
 import PlaylistItem from "../components/PlaylistItem";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const PLAYLIST = "PLAYLIST";
+
 const Playlist = () => {
   const [inputPlaylist, setInputPlaylist] = useState(""); // tên playlist mới
   const [playlistList, setPlaylistList] = useState([]); // danh sách playlist đã tạo
+
+  // lưu playlist mới vào Async Storage
+  const savePlaylist = async () => {
+    try {
+      await AsyncStorage.setItem(
+        PLAYLIST,
+        JSON.stringify([...playlistList, inputPlaylist])
+      );
+    } catch (e) {
+      alert("Failed to save the PLAYLIST to the storage");
+    }
+  };
+  // đọc danh sách playlist từ Async Storage
+  const readPlaylist = async () => {
+    try {
+      const value = await AsyncStorage.getItem(PLAYLIST);
+      if (value !== null) {
+        setPlaylistList(JSON.parse(value));
+      }
+    } catch (e) {
+      alert("Failed to fetch the PLAYLIST from storage");
+    }
+  };
+
+  useEffect(() => {
+    readPlaylist();
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -31,6 +62,7 @@ const Playlist = () => {
         <TextInput
           placeholder="Thêm danh sách phát mới"
           style={styles.playlistInput}
+          value = {inputPlaylist}
           onChangeText={(value) => {
             setInputPlaylist(value);
           }}
@@ -43,10 +75,11 @@ const Playlist = () => {
                 alert("Playlist đã tồn tại, vui lòng tạo playlist mới!");
               else {
                 setPlaylistList([...playlistList, inputPlaylist]);
+                setInputPlaylist("");
+                savePlaylist();
               }
             } else {
               alert("Nhập tên playlist mới!");
-              //alert(JSON.stringify(playlistList));
             }
           }}
         >
