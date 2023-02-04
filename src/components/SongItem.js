db7a5cb8db4a4295b7155fcb2e66eff59c4f1303
anhducbuui270802 <anhducbui272002@gmail.com>
2023-02-04 15:13:13 +0700

    design songItem

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -1,30 +1,101 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 
 import { Audio } from "expo-av";
 
+const getThumbnailText = (filename) => {
+  return filename[0];
+};
+
 const SongItem = ({ info }) => {
   const navigation = useNavigation();
   return (
-    <TouchableOpacity
-      style={{
-        width: "100%",
-        height: 60,
-        paddingVertical: 5,
-        paddingHorizontal: 20,
-        justifyContent: "space-around",
-      }}
-      onPress={() => {
-        navigation.navigate("Player", { info });
-      }}
-    >
-      <Text style={{ fontSize: 18 }}>{info.name}</Text>
-      <Text style={{ fontSize: 16, color: "gray" }}>{info.singer}</Text>
-    </TouchableOpacity>
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Player", { info });
+          }}
+        >
+          <View style={styles.leftContainer}>
+            <View style={styles.thumbnail}>
+              <Text style={styles.thumbnailText}>
+                {getThumbnailText(info.name)}
+              </Text>
+            </View>
+            <View style={styles.titleContainer}>
+              <Text numberOfLines={1} style={styles.title}>
+                {info.name}
+              </Text>
+              <Text style={styles.singerName}>{info.singer}</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.separator} />
+    </>
   );
 };
 
 export default SongItem;
 
-const styles = StyleSheet.create({});
+const { width } = Dimensions.get("window");
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignSelf: "center",
+    width: width - 80,
+  },
+  leftContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+  },
+  rightCotainer: {
+    flexBasis: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  thumbnail: {
+    height: 50,
+    flexBasis: 50,
+    backgroundColor: "#b6b8b9",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 25,
+  },
+  thumbnailText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#303d49",
+  },
+  titleContainer: {
+    width: width - 180,
+    paddingLeft: 10,
+  },
+  title: {
+    fontSize: 18,
+    color: "#303d49",
+  },
+  separator: {
+    width: width - 80,
+    backgroundColor: "#333",
+    opacity: 0.3,
+    height: 0.5,
+    alignSelf: "center",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  singerName: {
+    fontSize: 16,
+    color: "gray",
+  },
+});
